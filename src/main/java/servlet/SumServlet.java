e1dd4c250dd7be68f5cd61f3b4ec25b6095a1dbd
TH <h01581@haaga-helia.fi>
2020-04-29 10:34:31 +0300

    SumServlet kahden JSP-sivun avulla

--- src/main/java/servlet/SumServlet.java
+++ src/main/java/servlet/SumServlet.java
@@ -13,10 +13,20 @@ public class SumServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // välitetään pyyntö edelleen sivulle form.jsp
+        req.getRequestDispatcher("/WEB-INF/sum/form.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int a = Integer.parseInt(req.getParameter("a")); // 2
         int b = Integer.parseInt(req.getParameter("b")); // 3
 
-        // 2 + 3 = 5
-        resp.getWriter().println(a + " + " + b + " = " + (a + b));
+        // servletiltä voidaan välittää Java-arvoja JSP-sivulle _attribuutteina_
+        req.setAttribute("first", a);
+        req.setAttribute("second", b);
+
+        // välitetään pyyntö eteenpäin result.jsp-sivulle:
+        req.getRequestDispatcher("/WEB-INF/sum/result.jsp").forward(req, resp);
     }
 }
